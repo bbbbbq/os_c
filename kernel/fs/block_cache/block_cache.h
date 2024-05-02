af7fe39959214956d232605f75a6e616c0cf097e
2082291273@qq.com <2082291273@qq.com>
2024-05-02 06:01:14 -0400

    磁盘读写plic外部中断待完成

--- kernel/fs/block_cache/block_cache.h
+++ kernel/fs/block_cache/block_cache.h
@@ -31,6 +31,11 @@ struct BlockCache
   uint64_t ref; // 引用计数
 };
 
+int alloc3_desc(int *idx);
+void free_chain(int i);
+void free_desc(int i);
+int alloc_desc();
+void virtio_disk_rw(BlockCache *b, int write);
 void BlockCache_manager_init();
 BlockCache *BlockCache_find_block_by_index(BlockDevice *blockdevice, uint64_t index);
 void block_cache_release(BlockCache *b);
